dc696f265a01648b9b6cffc98a6a9895e4e540c4
Yunbo Zhang <th24060@bristol.ac.uk>
2025-10-16 12:59:36 +0100

    Update post_items_page.dart

--- application/lib/pages/post_items_page.dart
+++ application/lib/pages/post_items_page.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class PostItemsPage extends StatelessWidget {
+  const PostItemsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Post Items')),
+      body: const Center(child: Text('Post Items')),
+    );
+  }
+}
