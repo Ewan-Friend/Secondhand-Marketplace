5e6bded733b279b445f18f8145c5d07407efd9c8
Ewan Friend <pu24994@bristol.ac.uk>
2025-11-09 00:54:24 +0000

    added list_model and get_items()

---
+++ application/lib/models/item_model.dart
@@ -0,0 +1,28 @@
+class Item {
+  final String id;
+  final String sellerId;
+  final String title;
+  final double rating;
+  final double price;
+  // final String listingDate;
+
+  Item({
+    required this.id,
+    required this.sellerId,
+    required this.title,
+    required this.rating, 
+    required this.price,
+    // required this.listingDate
+  });
+
+  factory Item.fromJson(Map<String, dynamic> json){
+    return Item(
+      id:           json['id'] as String,
+      sellerId:     json['seller_id'] as String,
+      title:        json['title'] as String,
+      rating:       (json['rating'] as num).toDouble(),
+      price:        (json['price'] as num).toDouble(),
+      // listingDate:  json['listing_date'] as String,
+    );
+  }
+}
