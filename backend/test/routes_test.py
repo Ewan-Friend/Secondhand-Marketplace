3f792d75c180fdd0cd8f08cab25896185becd81a
Ewan Friend <pu24994@bristol.ac.uk>
2026-03-16 22:00:48 +0000

    task(tests): added conftest to define client

--- backend/test/routes_test.py
+++ backend/test/routes_test.py
@@ -2,15 +2,6 @@ import pytest
 from unittest.mock import patch, MagicMock
 from app import create_app
 
-
-@pytest.fixture
-def client():
-    app = create_app()
-    app.config["TESTING"] = True
-    with app.test_client() as client:
-        yield client
-
-
 def test_get_item_data_success(client):
     # Use a "mock" supabase for testting purposes
     with (
