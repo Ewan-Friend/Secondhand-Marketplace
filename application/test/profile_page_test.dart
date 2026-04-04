ed5297c613b3b5cc0ba39c6887a88376f8960020
filipek <filip.hrehovcik.yt@gmail.com>
2026-04-04 11:22:25 +0200

    bugfix(tests): added a main function to satisfie the test framework

--- application/test/profile_page_test.dart
+++ application/test/profile_page_test.dart
@@ -3,8 +3,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:application/pages/profile_page.dart';
 import 'package:application/services/api_service.dart';
 
-// initialiing a fake API service for testing purposes
+// initialising a fake API service for testing purposes
 class _FakeApiService extends APIService {
   @override
   Future<String> checkConnection() async => 'API OK';
+}
+
+void main() {
+  // TODO: Add tests here
 }
